0799f0ec31e8d83c1325b46a1bc2fcaf9748ade8
김태영 <kimtaeyoung@0Taes-Mac.local>
2021-11-25 22:29:55 +0900

    React Native Setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = async () => {
+    const fontsToLoad = [Ionicons.font];
+    console.log({ fontsToLoad });
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    console.log({ fontPromises });
+    const imagesToLoad = [
+      require('./assets/logo.png'),
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png',
+    ];
+    console.log({ imagesToLoad });
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    console.log({ imagePromises });
+    await Promise.all([...fontPromises, ...imagePromises]);
+  };
+
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
